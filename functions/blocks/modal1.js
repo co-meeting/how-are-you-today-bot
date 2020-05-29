fe1dff3bb257c63b53cff43b5173fa8a29016f80
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-05-29 17:54:25 +0900

    画像を４件追加

--- functions/blocks/modal1.js
+++ functions/blocks/modal1.js
@@ -158,5 +158,145 @@ const modal1 = [
   {
     type: "divider",
   },
+  {
+    type: "image",
+    title: {
+      type: "plain_text",
+      text: " ",
+      emoji: true,
+    },
+    image_url:
+      "https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-06.png",
+    alt_text: "Image6",
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "選択",
+          emoji: true,
+        },
+        value: "Image6",
+      },
+    ],
+  },
+  {
+    type: "divider",
+  },
+  {
+    type: "image",
+    title: {
+      type: "plain_text",
+      text: " ",
+      emoji: true,
+    },
+    image_url:
+      "https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-07.png",
+    alt_text: "Image7",
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "選択",
+          emoji: true,
+        },
+        value: "Image7",
+      },
+    ],
+  },
+  {
+    type: "divider",
+  },
+  {
+    type: "image",
+    title: {
+      type: "plain_text",
+      text: " ",
+      emoji: true,
+    },
+    image_url:
+      "https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-08.png",
+    alt_text: "Image8",
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "選択",
+          emoji: true,
+        },
+        value: "Image8",
+      },
+    ],
+  },
+  {
+    type: "divider",
+  },
+  {
+    type: "image",
+    title: {
+      type: "plain_text",
+      text: " ",
+      emoji: true,
+    },
+    image_url:
+      "https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-09.png",
+    alt_text: "Image9",
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "選択",
+          emoji: true,
+        },
+        value: "Image9",
+      },
+    ],
+  },
+  {
+    type: "divider",
+  },
+  {
+    type: "image",
+    title: {
+      type: "plain_text",
+      text: " ",
+      emoji: true,
+    },
+    image_url:
+      "https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-10.png",
+    alt_text: "Image10",
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "選択",
+          emoji: true,
+        },
+        value: "Image10",
+      },
+    ],
+  },
+  {
+    type: "divider",
+  },
 ];
 module.exports = modal1;
