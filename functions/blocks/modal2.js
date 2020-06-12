763b35daf0746ae75fbc804eef9315f0f63ff931
Atsuhiko Kimura <atskimura@gmail.com>
2020-06-12 16:18:13 +0900

    投稿完了画面に時間がかかる旨を記載

--- functions/blocks/modal2.js
+++ functions/blocks/modal2.js
@@ -3,7 +3,7 @@ const modal2 = () => [
     "type": "section",
     "text": {
       "type": "plain_text",
-      "text": "今日のひとことを投稿しました。"
+      "text": "今日のひとことを投稿しました。\n投稿されるまでに数分かかることがあります。"
     }
   }
 ]
