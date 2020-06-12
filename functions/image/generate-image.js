9ce70c4c422c0dbadb1cf56bf2c57bbeddb6f84b
Atsuhiko Kimura <atskimura@gmail.com>
2020-06-12 15:01:49 +0900

    画像内テキストの改行対応

--- functions/image/generate-image.js
+++ functions/image/generate-image.js
@@ -3,6 +3,7 @@ const { getConfig } = require('./config');
 
 const canvasWidth = 1200;
 const canvasHeight = 630;
+const padding = 50;
 
 const canvas = createCanvas(canvasWidth, canvasHeight)
 const ctx = canvas.getContext('2d')
@@ -15,21 +16,59 @@ function getNum() {
   return ('0' + num).slice(-2);
 }
 
+function splitByMeasureWidth(str) {
+  const maxWidth = canvasWidth - (padding * 2);
+
+  // サロゲートペアを考慮した文字分割
+  let chars = Array.from(str);
+  let line = '';
+  let lines = [];
+  chars.forEach((char, index) => {
+    if (maxWidth <= ctx.measureText(line + char).width) {
+      lines.push(line);
+      line = char;
+    }
+    else {
+      line += char;
+    }
+  });
+  lines.push(line);
+  return lines;
+}
+
+function renderText(text, style) {
+  ctx.fillStyle = style.fillStyle;
+  ctx.font = style.font;
+  ctx.textAlign = style.textAlign;
+
+  const lines = splitByMeasureWidth(text);
+  const boxHeight = lines.length * style.lineHeight;
+  lines.forEach((line, index) => {
+    ctx.fillText(line, canvasWidth/2, style.top + (style.lineHeight * index))
+  });
+}
+
 async function generateImage(question, answer) {
   const num = getNum();
   const config = getConfig(num);
   const image = await loadImage(`https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-${num}.png`);
   ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
 
-  ctx.font = `bold 48px ${ctx.font}`
-  ctx.fillStyle = config.fillStyle;
-  ctx.textAlign = config.textAlign;
-  ctx.fillText(question, canvasWidth/2, 120);
+  renderText(question, {
+    font: `bold 48px ${ctx.font}`,
+    lineHeight: 58,
+    fillStyle: config.fillStyle,
+    textAlign: config.textAlign,
+    top: 120
+  });
 
-  ctx.font = `bold 72px ${ctx.font}`
-  ctx.fillStyle = config.fillStyle;
-  ctx.textAlign = config.textAlign;
-  ctx.fillText(answer, canvasWidth/2, canvasHeight/2);
+  renderText(answer, {
+    font: `bold 72px ${ctx.font}`,
+    lineHeight: 84,
+    fillStyle: config.fillStyle,
+    textAlign: config.textAlign,
+    top: canvasHeight/2
+  });
 
   return canvas.createPNGStream();
 }
