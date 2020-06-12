80e95cd8d53df99d6adaaf58f5812eeb90060eb4
mcho71 <machida.city7110@gmail.com>
2020-06-12 13:59:07 +0900

    01~10が出るように修正

--- functions/image/generate-image.js
+++ functions/image/generate-image.js
@@ -6,16 +6,24 @@ const canvasHeight = 630;
 const canvas = createCanvas(canvasWidth, canvasHeight)
 const ctx = canvas.getContext('2d')
 
+function getNum() {
+  let num;
+  do {
+    num = Math.floor(Math.random() * 10) + 1;
+  } while (num > 10);
+  return ('0' + num).slice(-2);
+}
+
 async function generateImage(question, answer) {
-  const num = Math.floor(Math.random() * 9) + 1;
-  const image = await loadImage(`https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-0${num}.png`);
+  const num = getNum();
+  const image = await loadImage(`https://co-meeting.github.io/how-are-you-today-bot/images/how-are-you-${num}.png`);
   ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
 
   ctx.fillStyle = '#f7ffed';
   ctx.font = 'bold 48px "Noto Sans CJK JP"';
   ctx.textAlign = 'center';
   ctx.fillText(question, canvasWidth/2, 120);
-  
+
   ctx.fillStyle = '#f7ffed';
   ctx.font = 'bold 72px "Noto Sans CJK JP"';
   ctx.textAlign = 'center';
