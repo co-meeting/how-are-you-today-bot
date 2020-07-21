01341130664012ad46018b4451bd1c5e65168cb6
mcho71 <machida.city7110@gmail.com>
2020-07-21 09:08:11 +0900

    日本のタイムゾーンに合わせて質問が変わるよう修正

--- functions/questions/questions.js
+++ functions/questions/questions.js
@@ -2,24 +2,21 @@ const questions = require('./list');
 
 function getIndex(day) {
   let index = day % questions.length;
-  if ( index === questions.length ) {
+  if (index === questions.length) {
     return getIndex(day - questions.length);
   }
   return index;
 }
 
-const getQuestion = function (){
+const getQuestion = function () {
   // 順番に質問を表示
   var date1 = new Date(2020, 4, 29);  // 起点:2020/5/29
   var date2 = new Date();
-  date2.setHours(0);
-  date2.setMinutes(0);
-  date2.setSeconds(0);
-  date2.setMilliseconds(0);
+  // 日本のタイムゾーンに合わせる
+  date2 = date2.getTime() + 9 * 60 * 60 * 1000;
 
-  var diffDay = (date2 - date1) / (24 * 60 * 60 * 1000);
+  var diffDay = Math.floor((date2 - date1) / (24 * 60 * 60 * 1000));
   const index = getIndex(diffDay);
   return questions[index];
-  
 };
 module.exports = getQuestion;
